c9a778f376f7257eab868c6a431f39e8d35694d0
ManishBej <manishbej@gmail.com>
2025-02-10 20:28:54 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,10 @@ import { SeedService } from './seed/seed';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false
   });
   app.useGlobalPipes(new ValidationPipe());
   const seedService = app.get(SeedService);
